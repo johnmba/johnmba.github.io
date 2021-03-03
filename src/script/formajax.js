2057e5d3750aaeaf4c4d7d98db96ca4c95f4470d
john <nwanjajohnmba@gmail.com>
2021-03-03 22:29:18 +0100

    getting there

--- src/script/formajax.js
+++ src/script/formajax.js
@@ -269,3 +269,94 @@ log_btn.addEventListener('click', ()=>{
 	//log_form.classList.add('unhidden');
 	console.log(log_form)
 })
+
+
+
+// other js scripts to be used latter
+
+const leftArrow = document.querySelector('.arrows label[for="item1"]')
+const rightArrow = document.querySelector('.arrows label[for="item2"]')
+const indicatorParent = document.querySelector('.dot')
+var sectionIndex = 0
+
+function setIndex() {
+  document.querySelector('.arrows .selected').classList.remove('selected')
+  slider.style.transform = 'translate(' + sectionIndex * -25 + '%)';
+}
+
+var dots = document.querySelectorAll(".dot li label")
+console.log(dots)
+dots.forEach(function(indicator, ind){
+  indicator.addEventListener('click', function(){
+    sectionIndex = ind;
+    setIndex();
+    indicator.classList.add('selected')
+  });
+})
+
+leftArrow.addEventListener('click', function(){
+  sectionIndex = (sectionIndex > 0) ?  sectionIndex - 1 : 0;
+  indicatorParent.children[sectionIndex].classList.remove('selected');
+  setIndex();
+})
+console.log(leftArrow)
+
+leftArrow.addEventListener('click', function(){
+  sectionIndex = (sectionIndex > 3) ?  sectionIndex + 1 : 0;
+  indicatorParent.children[sectionIndex].classList.remove('selected');
+  setIndex();
+})
+
+const portfolios = document.querySelectorAll('.portfolio-item-wrapper')
+portfolios.forEach(portfolio =>{
+  portfolio.addEventListener('mouseover', () =>{
+    portfolio.childNodes[1].classList.add('bg-darken')
+  });
+  portfolio.addEventListener('mouseout', () =>{
+    portfolio.childNodes[1].classList.remove('bg-darken')
+  });
+})
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
--- src/script/genJs.js
+++ src/script/genJs.js
@@ -1,86 +1,25 @@
-const leftArrow = document.querySelector('.arrows label[for="item1"]')
-const rightArrow = document.querySelector('.arrows label[for="item2"]')
-const indicatorParent = document.querySelector('.dot')
-var sectionIndex = 0
 
-function setIndex() {
-  document.querySelector('.arrows .selected').classList.remove('selected')
-  slider.style.transform = 'translate(' + sectionIndex * -25 + '%)';
-}
+const portfolioMenus = document.querySelectorAll('.portfolio-menu .menu-item')
+const portfolioItems = document.querySelectorAll('.portfolio-item')
 
-var dots = document.querySelectorAll(".dot li label")
-console.log(dots)
-dots.forEach(function(indicator, ind){
-  indicator.addEventListener('click', function(){
-    sectionIndex = ind;
-    setIndex();
-    indicator.classList.add('selected')
+const info = document.querySelector('.info')
+
+//info.addEventListener('click', ()=>{ 
+  //console.log(item)
+  //info.classList.remove('hidden')
+//})
+
+for (let item = 0; item < portfolioMenus.length; item++) {
+  const portfolioMenu = portfolioMenus[item];
+  portfolioMenu.addEventListener('mouseover', ()=>{ 
+    info.classList.add('hidden')
+    portfolioItems[item].classList.remove('portfolio-item')
+    //portfolioItems[item].style.display = 'flex'
   });
-})
-
-leftArrow.addEventListener('click', function(){
-  sectionIndex = (sectionIndex > 0) ?  sectionIndex - 1 : 0;
-  indicatorParent.children[sectionIndex].classList.remove('selected');
-  setIndex();
-})
-console.log(leftArrow)
-
-leftArrow.addEventListener('click', function(){
-  sectionIndex = (sectionIndex > 3) ?  sectionIndex + 1 : 0;
-  indicatorParent.children[sectionIndex].classList.remove('selected');
-  setIndex();
-})
-
-const portfolios = document.querySelectorAll('.portfolio-item-wrapper')
-portfolios.forEach(portfolio =>{
-  portfolio.addEventListener('mouseover', () =>{
-    portfolio.childNodes[1].classList.add('bg-darken')
+  console.log(portfolioItems[item]);
+  portfolioMenu.addEventListener('mouseout', ()=>{ 
+    info.classList.remove('hidden')
+    portfolioItems[item].classList.add('portfolio-item')
+    //portfolioItems[item].style.display = 'none'
   });
-  portfolio.addEventListener('mouseout', () =>{
-    portfolio.childNodes[1].classList.remove('bg-darken')
-  });
-})
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
